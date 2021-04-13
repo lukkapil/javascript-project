11436c3ec8192c1c421b27f71588434b327eaaea
Łukasz <lukaszpilch31@gmail.com>
2021-04-13 12:40:14 +0200

    Modal -> done

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+function closeModal(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+function openModal(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+for(let i=0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// nie używamy wywołania w ten sposób closeModal() bo to wywołałoby funkcję od razu, a my
+// chcemy żeby wywołała się po kliknięciu
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+// parametr event przekazywany w funkcji to po prostu obiekt tworzony przez JS
+// można go nazwać jakkolwiek
+// kiedy ten 'event' będzie miał miejsce, to JS wywoła tą funkcję
+// po ludzku "Hej JavaScript, wywołaj tą funkcje wtedy kiedy event się wydarzy, jak się 
+// wydarzy, to przekaż event jako argument"
+document.addEventListener('keydown', function(event){
+    console.log(event.key);
+    if(event.key === 'Escape' && !modal.classList.contains('hidden')){
+        // takie zamknięcie modala mogło by zadziałać w cypressie przy przedłużeniu sesji
+            closeModal();
+    }
+})
